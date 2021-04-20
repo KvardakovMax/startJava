ccce8cebd3af575768653539f62d45824fd857e0
Max Kvardakov <jimbomaxkvr@gmail.com>
2021-04-20 17:32:42 +0300

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+    byte cpuCores = 8;
+    short numberOfVideoCards = 2;
+    int numberOfMonitors = 2;
+    long numberOfFolders = 1000000;
+    float cpuFrequency = 3.9f;
+    double computerCoast = 1500.5;
+    char[] brand = {'R', 'y', 'z', 'e', 'n'};
+    boolean myPC = true;
+    System.out.println(cpuCores);
+    System.out.println(numberOfVideoCards);
+    System.out.println(numberOfMonitors);
+    System.out.println(numberOfFolders);
+    System.out.println(cpuFrequency);
+    System.out.println(computerCoast);
+    System.out.println(brand);
+    System.out.println(myPC);
+    }
+}
