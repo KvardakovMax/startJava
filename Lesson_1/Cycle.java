7f3d28ed71b314827d26c5e2e07d256ddeb665f4
Max Kvardakov <jimbomaxkvr@gmail.com>
2021-04-27 14:33:44 +0300

    Fixed comments 2

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,23 +1,23 @@
 public class Cycle {
     public static void main(String[] args) {
-        for(int i =0; i <= 20; i++) {
+        for(int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
 
-        int i =6;
+        int i = 6;
         while(i >= -6) {
             System.out.println(i);
             i -= 2;
         }
 
-        int number = 10;
-        int result = 0;
+        int counter = 10;
+        int sumOdd = 0;
         do {
-            if(number % 2 != 0) {
-                result += number;
+            if(counter % 2 != 0) {
+                sumOdd += counter;
             }
-            number++;
-        } while(number < 20);
-        System.out.println(result);
+            counter++;
+        } while(counter < 20);
+        System.out.println(sumOdd);
     }
 }
