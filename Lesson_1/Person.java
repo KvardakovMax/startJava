5fc8d8ee1e070cf080fef5304665ebf46c0c63f6
Max Kvardakov <jimbomaxkvr@gmail.com>
2021-05-11 23:24:35 +0300

    HW Person add

---
+++ Lesson_1/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    char gender = 'M';
+    String name = "Jon";
+    int height = 175;
+    int weight = 75;
+    int age = 20;
+
+    public void go() {
+
+    }
+    public void sit() {
+
+    }
+    public void run() {
+
+    }
+    public void say() {
+
+    }
+    public void learnJava() {
+
+    }
+}
