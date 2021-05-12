57368592cad527f81b733993c7a02b784cf29e58
Max Kvardakov <jimbomaxkvr@gmail.com>
2021-05-12 23:17:02 +0300

    Wolf and Person HW uploaded to folder Lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    char gender = 'M';
+    String name = "Jon";
+    int height = 175;
+    int weight = 75;
+    int age = 20;
+
+    public void go() {
+    }
+
+    public void sit() {
+    }
+
+    public void run() {
+    }
+
+    public void say() {
+    }
+
+    public void learnJava() {
+    }
+}
---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,26 @@
+public class Wolf {
+    char gender;
+    String name;
+    int weight;
+    int age;
+    int colore;
+    public void go() {
+        System.out.println("Идет");
+    }
+
+    public void sit() {
+        System.out.println("Сидит");
+    }
+
+    public void run() {
+        System.out.println("Бежит");
+    }
+
+    public void howl() {
+        System.out.println("Воет");
+    }
+
+    public void hunt() {
+        System.out.println("Охотится");
+    }
+}
