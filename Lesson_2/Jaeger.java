1b54312a29f02f0d2c6d313106669e2f76b1013f
Max Kvardakov <jimbomaxkvr@gmail.com>
2021-07-01 22:57:21 +0300

    1 version

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,76 @@
+public class Jaeger {
+        private String modelName;
+    private String mark;
+    private String origin;
+    private String weapon;
+    private String yearOfLaunched;
+    private float height;
+    private float weight;
+    private int strength;
+    private int armor;
+
+    public boolean drift() {
+        return true;
+    }
+
+    public void move() {
+        System.out.println("moving");
+    }
+
+    public String scanKaiju() {
+        return "scanning";
+    }
+
+    public void useVortexCannon() {
+        System.out.println("activated");
+    }
+
+    public void activateEnergyBeam() {
+        System.out.println("Energy Beam is activated");
+    }
+
+    public void setChooseWeapon(String weapon) {
+        this.weapon = weapon;
+        System.out.println(weapon);
+    }
+
+    public void setSayWhenYouWereLaunched (String yearOfLaunched) {
+        this.yearOfLaunched = yearOfLaunched;
+        System.out.println(yearOfLaunched);
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+        System.out.println(modelName);
+    }
+    
+    public void setMark(String mark) {
+        this.mark = mark;
+        System.out.println(mark);
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+        System.out.println(origin);
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+        System.out.println("Высота: " + height);
+    }
+
+    public void setWeight(float weight) {
+        this.weight =weight;
+        System.out.println(weight);
+    }
+    
+    public void setStrength(int strength)  {
+        this.strength = strength;
+        System.out.println(strength);
+    }
+    
+    public void setArmor(int armor) {
+        this.armor = armor;
+        System.out.println(armor);
+    }
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,17 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new jaeger();
+        Jaeger jaeger2 = new jaeger();
+
+        jaeger1.setModelName("Apex");
+
+        jaeger1.setSayWhenYouWereLaunched("2035");
+
+        jaeger1.setChooseWeapon("Energy Beam");
+
+        jaeger2.setModelName("Drone Jaeger");
+
+        jaeger2.setHeihgt(3);
+
+    }
+}
