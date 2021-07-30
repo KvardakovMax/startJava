09ace547cc2cf6f435fa20d08e9a31eef3a02998
Max Kvardakov <jimbomaxkvr@gmail.com>
2021-07-30 13:39:30 +0300

    New version GuessNumberTest

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        String player1Name;
-        String player2Name;
-        String answer = "yes";
         Scanner in = new Scanner(System.in);
+        String answer = "yes";
+
 
         System.out.println("Игрок 1, введите свой ник: ");
-        player1Name = in.nextLine();
+        String player1Name = in.nextLine();
         Player player1 = new Player(player1Name);
 
         System.out.println("Игрок 2, введите свой ник: ");
-        player2Name = in.nextLine();
+        String player2Name = in.nextLine();
         Player player2 = new Player(player2Name);
 
         GuessNumber game = new GuessNumber(player1, player2);
@@ -23,7 +22,13 @@ public class GuessNumberTest {
             System.out.println("Хотите продолжить игру? [yes/no]: ");
             answer = in.nextLine();
         }
-        System.out.println("END");
+        while (answer.equals("no")) {
+            System.out.println("END");
+        }
+        while (!answer.equals("yes") || !answer.equals("no")) {
+            System.out.println("Хотите продолжить игру? [yes/no]: ");
+            answer = in.nextLine();
+        }
 
 
 
